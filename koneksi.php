a358d6819e5114f7dadfb3f28b414fbedd2c99c8
Deden.kurnia <nktpamungkas28@gmail.com>
2024-04-23 14:20:17 +0700

    penambahan lot legacy

--- koneksi.php
+++ koneksi.php
@@ -22,8 +22,8 @@ else{
     exit("DB2 Connection failed");
 }
 
-// $con=mysqli_connect("10.0.0.10","dit","4dm1n","db_qc");
-$con=mysqli_connect("localhost","root","","db_qc");
+$con=mysqli_connect("10.0.0.10","dit","4dm1n","db_qc");
+// $con=mysqli_connect("localhost","root","","db_qc");
 $condye=mysqli_connect("10.0.0.10","dit","4dm1n","db_dying");
 $conlab=mysqli_connect("10.0.0.10","dit","4dm1n","db_laborat");
 if (mysqli_connect_errno()) {
