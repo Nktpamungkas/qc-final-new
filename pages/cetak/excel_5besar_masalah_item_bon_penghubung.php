8fd1b91c883908dc4d0342dde1013f7a112b2df5
hadi.subhana <hadi.subhana@indotaichen.com>
2024-07-04 09:04:58 +0700

    aktifkan cetak excel lap 5 besar bon penghubung

---
+++ pages/cetak/excel_5besar_masalah_item_bon_penghubung.php
@@ -0,0 +1,183 @@
+<?php
+error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+header("Content-type: application/octet-stream");
+header("Content-Disposition: attachment; filename=5Besar-Masalah-PerHanger-BonPenghubung-".substr($_GET['awal'],0,10).".xls");//ganti nama sesuai keperluan
+header("Pragma: no-cache");
+header("Expires: 0");
+//disini script laporan anda
+?>
+<?php
+include "../../koneksi.php";
+
+$Awal = $_GET['awal'];
+$Akhir = $_GET['akhir'];
+$Langganan = $_GET['langganan'];
+$TotalKirim = $_GET['kirim'];
+?>
+
+<body>
+
+    <strong>Periode: <?php echo $Awal; ?> s/d <?php echo $Akhir; ?></strong><br>
+    <?php
+    if ($Langganan != '') {
+        $lgn = " AND tq.pelanggan LIKE '%$Langganan%' ";
+    }
+
+    $query = mysqli_query($con, "select
+                                                tq.*
+                                            from
+                                                tbl_qcf tq
+                                            where
+                                                tq.sts_pbon != '10'
+                                                AND DATE_FORMAT( tq.tgl_masuk, '%Y-%m-%d' ) BETWEEN '$Awal' AND '$Akhir'
+                                                and (tq.penghubung_masalah != ''
+                                                    or tq.penghubung_keterangan != ''
+                                                    or tq.penghubung_roll1 != ''
+                                                    or tq.penghubung_roll2 != ''
+                                                    or tq.penghubung_roll3 != ''
+                                                    or tq.penghubung_dep != ''
+                                                    or tq.penghubung_dep_persen != '') $lgn
+                                            group by
+                                                tq.no_order,
+                                                tq.no_po,
+                                                tq.no_hanger,
+                                                tq.no_item,
+                                                tq.warna,
+                                                tq.pelanggan");
+
+    $data = [];
+    while ($row = mysqli_fetch_assoc($query)) {
+        if ($row['penghubung2_roll1'] and $row['penghubung2_roll1'] != '') {
+            $row['qty_kg'] = $row['penghubung2_roll2'];
+            $row['penghubung_masalah_x'] = $row['penghubung2_masalah'];
+            $data[] = $row;
+        }
+        if ($row['penghubung3_roll1'] and $row['penghubung3_roll1'] != '') {
+            $row['qty_kg'] = $row['penghubung3_roll2'];
+            $row['penghubung_masalah_x'] = $row['penghubung3_masalah'];
+            $data[] = $row;
+        }
+        $row['qty_kg'] = $row['penghubung_roll2'];
+        $row['penghubung_masalah_x'] = $row['penghubung_masalah'];
+        $data[] = $row;
+    }
+
+    $groupHanger = [];
+    $sumQtyHanger = [];
+    foreach ($data as $key => $value) {
+        $hanger = $value['no_hanger'];
+        if (!isset($groupHanger[$hanger])) {
+            $groupHanger[$hanger] = [];
+            $sumQtyHanger[$hanger] = 0;
+        }
+        $groupHanger[$hanger][$key] = $value;
+        $sumQtyHanger[$hanger] += $value['qty_kg'] ?: 0;
+    }
+    arsort($sumQtyHanger);
+    $top5Hanger = array_slice($sumQtyHanger, 0, 5);
+
+    $penghubungMasalah = [];
+    foreach ($top5Hanger as $key => $value) {
+        $tempPenghubungMasalah = [];
+        foreach ($data as $key2 => $value2) {
+            $penghubung = $value2['penghubung_masalah_x'];
+            if ($key == $value2['no_hanger']) {
+                if (!isset($tempPenghubungMasalah[$penghubung])) {
+                    $tempPenghubungMasalah[$penghubung]['qty_kg'] = 0;
+                    $tempPenghubungMasalah[$penghubung]['jumlah_masalah'] = 0;
+                }
+                $tempPenghubungMasalah[$penghubung]['qty_kg'] += $value2['qty_kg'] ?: 0;
+                $tempPenghubungMasalah[$penghubung]['jumlah_masalah'] += 1;
+            }
+        }
+        arsort($tempPenghubungMasalah);
+        $penghubungMasalah[$key] = array_slice($tempPenghubungMasalah, 0, 3);
+    }
+
+    // 5 hanger terbesar
+    $HangerTerbesar = [];
+    foreach ($penghubungMasalah as $key => $value) {
+        $tempTotalQtyKg = 0;
+        $tempTotalJumlahMasalah = 0;
+        foreach ($value as $key2 => $value2) {
+            $tempTotalQtyKg += $value2['qty_kg'];
+            $tempTotalJumlahMasalah += $value2['jumlah_masalah'];
+        }
+        $HangerTerbesar[$key]['total_kg'] = $tempTotalQtyKg;
+        $HangerTerbesar[$key]['total_jumlah_masalah'] = $tempTotalJumlahMasalah;
+    }
+
+    $totalKeluhanKG = 0;
+    foreach ($HangerTerbesar as $key => $value) {
+        $totalKeluhanKG += $value['total_kg'];
+    }
+
+    ?>
+    <table border="1">
+        <thead>
+            <tr>
+                <th width="15%">
+                    <div align="center">Hanger</div>
+                </th>
+                <th width="20%">
+                    <div align="center">Masalah</div>
+                </th>
+                <th width="20%">
+                    <div align="center">Jumlah Kasus</div>
+                </th>
+                <th width="14%">
+                    <div align="center">KG</div>
+                </th>
+                <th width="15%">
+                    <div align="center">% Dibandingkan Total Bon Penghubung</div>
+                </th>
+                <th width="15%">
+                    <div align="center">% Dibandingkan Total Kirim</div>
+                </th>
+                <th width="15%">
+                    <div align="center">% Masalah Per Hanger</div>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($top5Hanger as $key => $value) {
+                $satu = 1;
+                foreach ($penghubungMasalah[$key] as $key2 => $value2) {
+                    ?>
+                    <tr valign="top" <?= $satu > 0 ? 'style="border-top:2px solid #aaa;"' : ''; ?>>
+                        <td align="center"><?= $satu > 0 ? $key : '' ?></td>
+                        <td align="right"><?= $key2 ?></td>
+                        <td align="right"><?= $value2['jumlah_masalah'] ?></td>
+                        <td align="right"><?= $value2['qty_kg'] ?></td>
+                        <td align="right">
+                            <?= ($TotalKirim != '') ? number_format(($value2['qty_kg'] / $totalKeluhanKG) * 100, 2) . " %" : "0"; ?>
+                        </td>
+                        <td align="right">
+                            <?= ($TotalKirim != '') ? number_format(($value2['qty_kg'] / $TotalKirim) * 100, 2) . " %" : "0"; ?>
+                        </td>
+                        <td align="right">
+                            <?= ($TotalKirim != '') ? number_format(($value2['qty_kg'] / $value) * 100, 2) . " %" : "0"; ?>
+                        </td>
+                    </tr>
+                    <?php
+                    $satu--;
+                }
+            }
+            ?>
+        </tbody>
+        <tfoot>
+            <tr valign="top">
+                <td align="center" colspan="1"><strong>TOTAL KIRIM</strong></td>
+                <td align="right" colspan="1">
+                    <strong><?= ($TotalKirim != "") ? number_format($TotalKirim, 2) : "0"; ?></strong>
+                </td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </tfoot>
+    </table>
+</body>
