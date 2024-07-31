0668f9eb06487e983e6a614412638f68883fbb86
hadi.subhana <hadi.subhana@indotaichen.com>
2024-07-31 13:51:21 +0700

    singkat nama test

--- pages/cetak/cetak_label_list_test.php
+++ pages/cetak/cetak_label_list_test.php
@@ -3,6 +3,55 @@ ini_set("error_reporting", 1);
 session_start();
 include "../../koneksi.php";
 
+$singkatan = [
+  "FLAMMABILITY" => "Flam",
+  "FIBER CONTENT" => "Fiber Co",
+  "FABRIC COUNT" => "Fabric Co",
+  "BOW & SKEW" => "Bow-Skew",
+  "PILLING MARTINDLE" => "Pill Mar",
+  "FABRIC WEIGHT & SHRINKAGE & SPIRALITY" => "fw & shrink-Spr",
+  "PILLING BOX" => "Pill Box",
+  "PILLING RANDOM TUMBLER" => "Pill Rand",
+  "ABRATION" => "Abration",
+  "SNAGGING MACE" => "Snagg Mace",
+  "SNAGGING POD" => "Snagg Pod",
+  "BEAN BAG" => "Bean Bag",
+  "BURSTING STRENGTH" => "Burst Strength",
+  "THICKNESS" => "Thickness",
+  "STRETCH & RECOVERY" => "S & R",
+  "GROWTH" => "Growth",
+  "APPEARANCE" => "Apperance",
+  "HEAT SHRINKAGE" => "Heat Shrink",
+  "FIBRE/FUZZ" => "Fibre/Fuzz",
+  "PILLING LOCUS" => "Pill Loc",
+  "ODOUR" => "Odour",
+  "CURLING" => "Curling",
+  "NEDLE" => "NedHol & crock",
+  "WICKING" => "Wicking",
+  "ABSORBENCY" => "Absorbency",
+  "DRYING TIME" => "Dry Time",
+  "WATER REPPELENT" => "Water Repp",
+  "PH" => "PH",
+  "SOIL RELEASE" => "Soil Release",
+  "HUMIDITY" => "Humidity",
+  "WASHING" => "Wash Fast",
+  "PERSPIRATION ACID" => "Pers Fast Acid",
+  "PERSPIRATION ALKALINE" => "Pers Fast Alkali",
+  "WATER" => "Water Fast",
+  "CROCKING" => "Crock Fast",
+  "LIGHT" => "Light Fast",
+  "COLOR MIGRATION-OVEN TEST" => "C migration -OT",
+  "COLOR MIGRATION" => "C Migration Fast",
+  "LIGHT PERSPIRATION" => "Light Pers",
+  "SALIVA" => "Saliva Fast",
+  "BLEEDING" => "Bleeding",
+  "CHLORIN & NON-CHLORIN" => "Chlo & N-Ch",
+  "DYE TRANSFER" => "Dye Trans",
+  "SWEAT CONCEAL" => "Sweat Conceal",
+  "PHENOLIC YELLOWING" => "Pehn Yellow",
+  "WRINKLE" => "Wrinkle"
+];
+
 $idkk = $_REQUEST['idkk'];
 
 $data = mysqli_query($con, "select
@@ -44,7 +93,7 @@ $iii = 0;
   <style>
     td {
       border: none !important;
-      font-size: 8px !important;
+      font-size: 7px !important;
       vertical-align: top !important;
     }
   </style>
@@ -55,14 +104,14 @@ $iii = 0;
   <table width="100%" border="0" style="width: 7in; border-collapse: collapse">
     <tbody>
       <tr>
-        <td align="left" valign="top" style="height: 1.6in; border: 1px solid #000 !important;">
+        <td align="left" valign="top" style="height: 1.6in;;">
           <table width="100%" border="0" class="table-list1" style="width: 2.3in;">
             <tr>
               <td colspan="3">
                 <table>
                   <?php while ($i < 10) { ?>
                     <tr>
-                      <td><?= $r[$i++] ?></td>
+                      <td><?= $singkatan[$r[$i++]] ?></td>
                     </tr>
                   <?php } ?>
                 </table>
@@ -71,7 +120,7 @@ $iii = 0;
                 <table>
                   <?php while ($i < 20) { ?>
                     <tr>
-                      <td><?= $r[$i++] ?></td>
+                      <td><?= $singkatan[$r[$i++]] ?></td>
                     </tr>
                   <?php } ?>
                 </table>
@@ -80,14 +129,14 @@ $iii = 0;
           </table>
         </td>
 
-        <td align="left" valign="top" style="height: 1.6in; border: 1px solid #000 !important">
+        <td align="left" valign="top" style="height: 1.6in;">
           <table width="100%" border="0" class="table-list1" style="width: 2.3in;">
             <tr>
               <td colspan="3">
                 <table>
                   <?php while ($ii < 10) { ?>
                     <tr>
-                      <td><?= $r[$ii++] ?></td>
+                      <td><?= $singkatan[$r[$ii++]] ?></td>
                     </tr>
                   <?php } ?>
                 </table>
@@ -96,7 +145,7 @@ $iii = 0;
                 <table>
                   <?php while ($ii < 20) { ?>
                     <tr>
-                      <td><?= $r[$ii++] ?></td>
+                      <td><?= $singkatan[$r[$ii++]] ?></td>
                     </tr>
                   <?php } ?>
                 </table>
@@ -105,14 +154,14 @@ $iii = 0;
           </table>
         </td>
 
-        <td align="left" valign="top" style="height: 1.6in; border: 1px solid #000 !important">
+        <td align="left" valign="top" style="height: 1.6in;">
           <table width="100%" border="0" class="table-list1" style="width: 2.3in;">
             <tr>
               <td colspan="3">
                 <table>
                   <?php while ($iii < 10) { ?>
                     <tr>
-                      <td><?= $r[$iii++] ?></td>
+                      <td><?= $singkatan[$r[$iii++]] ?></td>
                     </tr>
                   <?php } ?>
                 </table>
@@ -121,7 +170,7 @@ $iii = 0;
                 <table>
                   <?php while ($iii < 20) { ?>
                     <tr>
-                      <td><?= $r[$iii++] ?></td>
+                      <td><?= $singkatan[$r[$iii++]] ?></td>
                     </tr>
                   <?php } ?>
                 </table>
