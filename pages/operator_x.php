f26fd76258746d86854ec65157926cb4cb0a53e9
hadi.subhana <hadi.subhana@indotaichen.com>
2024-11-06 10:56:17 +0700

    kg dan yard terisi jika operator di pilih

---
+++ pages/operator_x.php
@@ -0,0 +1,25 @@
+<?php
+include("../koneksi.php");
+
+$sqlYard = "SELECT 
+                SUM(LENGTHGROSS) AS LENGTHGROSS
+            FROM ELEMENTSINSPECTION
+            WHERE DEMANDCODE ='$_GET[demand]' AND OPERATORCODE = '$_GET[selectedValue]' ";
+$stmtYard = db2_exec($conn1, $sqlYard, array('cursor' => DB2_SCROLLABLE));
+$rowYard = db2_fetch_assoc($stmtYard);
+
+$sqlKonversi = "SELECT
+                SECONDARYUNSTEADYCVSFACTOR
+            FROM ITXVIEW_NETTO
+            WHERE CODE ='$_GET[demand]' ";
+$stmtKonversi = db2_exec($conn1, $sqlKonversi, array('cursor' => DB2_SCROLLABLE));
+$rowKonversi = db2_fetch_assoc($stmtKonversi);
+
+$KG = $rowYard['LENGTHGROSS'] * $rowKonversi['SECONDARYUNSTEADYCVSFACTOR'];
+
+$response['yard'] = sprintf("%.2f", $rowYard['LENGTHGROSS']);
+$response['kg'] = sprintf("%.2f", $KG);
+
+echo json_encode($response);
+
+?>
